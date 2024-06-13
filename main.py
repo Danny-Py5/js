ab827db3d8f20a929a46664dcb9ea74e58430a2b
DannyPy <olatundedaniel943@gmail.com>
2024-06-13 13:28:43 -0500

    initial

---
+++ main.py
@@ -0,0 +1,99 @@
+import random
+
+
+class RockPaperScissors:
+
+    def __init__(self):
+
+        self.computer_choose = random.choice((1,2,3))
+        self.result = ''
+
+    def choose(self):
+        ch = ''
+        if self.computer_choose == 1:
+            ch = 'rock'
+        elif self.computer_choose == 2:
+            ch = 'paper'
+        else:
+            ch = 'scissors'
+        return ch
+    
+    def info(self, player, computer_ch, msg):
+        return f'You choose {player}, computer choose {computer_ch} {msg}'
+    
+    def compere(self, val:str):
+        computer = self.choose()
+        if val == 'rock':
+            if computer == 'rock':
+                self.result = self.info(val, computer, 'Tie')
+            elif computer == 'paper':
+                self.result = self.info(val, computer, 'You lose')
+            else:
+                self.result = self.info(val, computer, 'You win.')
+                
+        elif val == 'paper':
+            if computer == 'rock':
+                self.result = self.info(val, computer, 'You win.')
+            elif computer == 'paper':
+                self.result = self.info(val, computer, 'Tie')
+            else:
+                self.result = self.info(val, computer, 'You lose')
+        else:
+            if computer == 'rock':
+                self.result = self.info(val, computer, 'You lose')
+            elif computer == 'paper':
+                self.result = self.info(val, computer, 'You win')
+            else:
+                self.result = self.info(val, computer, 'Tie')
+
+    def display(self, val:str):
+        self.compere(val)
+        return self.result
+
+def main():
+    while True:
+        rps = RockPaperScissors()
+
+        print('1. Rock\n2. Paper\n3. Scissors')
+        player = input('Choose: ').lower()
+        if player in ('1','2','3','rock','paper','scissors'):
+            print(rps.display(player.replace('1','rock').replace('2','paper').replace('3','scissors')))
+        else:
+            print('invalid input try again!')
+            continue
+        
+        if input('Play again y/n: ') != 'y':
+            break
+
+a = True if 55 > 5 else False
+
+
+if __name__ == '__main__':
+    # main()
+    pass
+
+
+new  = [4, 5, 6]
+
+
+new.pop(1)
+
+print(new)
+
+
+for val in range(10):
+    if val == 5:
+        continue
+    print(val)
+
+
+ls = range(-7, 10, 2)
+
+print(list(filter(lambda x: x > 0, ls)))
+
+print(list(map(lambda x: x + 1, ls)))
+
+print(list((x + 1 for x in ls)))
+
+
+
---
+++ scripts/11-todo-list.js
@@ -0,0 +1,129 @@
+`Steps (Algorithm)
+1. create array to store todos
+2. when we click 'Add', 
+3. Get text form textbox
+4. Add it to array 
+5. console.log() the array`
+
+let todoList = getTodoFromLocalStorage() || [{}]
+// console.log(todoList);
+
+// localStorage.setItem('todo', JSON.stringify([{
+//     name: 'make dinner',
+//     duedate: '2022-12-22',
+// }, {
+//     name: 'buy beans',
+//     duedate: '2022-12-25',
+// }]));
+
+// console.log(JSON.parse(localStorage.getItem('todo')))
+
+
+displayTodo();
+function displayTodo(){
+    /*displays todolist on the webpage*/
+    let todoContainerElem = document.querySelector('.js-todo-container');
+    let todolistHTML = ''
+    // add all todolist to the todocontainer on the web page
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        const {name, duedate} = todoObject;
+        todolistHTML += `
+        <section style="max-width: 90%; ">${name}</section>
+        <section>${duedate}</section>
+        <button class="js-delete-todo-button">Delete</button>`;
+    };
+    todoContainerElem.innerHTML = todolistHTML;
+    // add eventlistener to the delete button
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(index, 1)
+                removeTodoFromLocalStorage(index);
+                displayTodo();
+            });
+        });
+
+    // console.log(todolistHTML)
+    if (todoContainerElem.innerHTML === '') {
+        todoContainerElem.style.display = 'block';
+        todoContainerElem.innerHTML = '<p class="no-todo-from-js" style="text-align:center;">Todo List is empty</p>'
+
+    }else{
+        todoContainerElem.style.display = 'grid'
+    };
+};
+
+
+function resetTodoNameInputElem() {
+    const todoNameElem = document.querySelector('.js-todoname');
+    todoNameElem.value = '';
+};
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||
+// local storage functions 
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+function saveToLocalStorage(todo){
+    todoList.push(todo);
+    //  add the updated todolist into localstorage back
+    localStorage.removeItem('todo');
+    localStorage.setItem('todo', JSON.stringify(todoList));
+};
+
+function getTodoFromLocalStorage(){
+    // console.log(localStorage.getItem('todo'));
+    const todos = JSON.parse(localStorage.getItem('todo'));
+    return todos
+};
+
+function removeTodoFromLocalStorage(todoIndex){
+    // remove from todolist the todo
+    localStorage.removeItem('todo');
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    // console.log(todoIndex)
+};
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||
+// local storage functions ends
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+function getTodoAndDate(){
+    // get todoname and date elem
+    const todoNameElem = document.querySelector('.js-todoname');
+    const dateInputElem = document.querySelector('.js-date-input');
+
+    // get the todoname and date value
+    const name = todoNameElem.value;
+    const duedate = dateInputElem.value;
+
+    return {name, duedate};
+};
+
+function handleEnter(event) {
+    const {name, duedate} = getTodoAndDate();
+    if (event.key==='Enter' && name.length) {
+        // console.log(todoList)
+        saveToLocalStorage({name, duedate});
+        resetTodoNameInputElem()
+        displayTodo();
+    };
+};
+
+function addTodo() {
+    /* add todolist to todolist array when the add button is clicked. */
+    const {name, duedate} = getTodoAndDate();
+    // return if name is ''
+    if (!name.length) {
+        return;
+    }
+    // add into array
+    saveToLocalStorage({name, duedate});
+    displayTodo();
+    resetTodoNameInputElem();
+    // console.log(todoList)
+}
+
+
+
